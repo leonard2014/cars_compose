29adcecdcc9d8112edcf5e8b4388c64cc2e24ab0
leo <leoxwooaus@gmail.com>
2022-03-01 14:30:15 +1100

    refactor

--- app/src/main/java/com/leonard/cars_compose/ui/CarsApp.kt
+++ app/src/main/java/com/leonard/cars_compose/ui/CarsApp.kt
@@ -1,19 +1,16 @@
 package com.leonard.cars_compose.ui
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.leonard.cars_compose.R
-import com.leonard.cars_compose.ui.model.CarsUIItem
-import com.leonard.cars_compose.ui.model.CarsUIList
 import com.leonard.cars_compose.ui.model.UIState
 import com.leonard.cars_compose.ui.theme.Cars_Theme
 
@@ -44,35 +41,6 @@ fun CarsApp(mainViewModel: MainViewModel) {
             }
         }
     }
-}
-
-@Composable
-private fun CarsList(list: CarsUIList) {
-    LazyColumn(
-        contentPadding = PaddingValues(16.dp),
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.spacedBy(16.dp)
-    ) {
-        items(items = list) { car ->
-            ListRow(car)
-            Divider()
-        }
-    }
-}
-
-@Composable
-@Preview
-private fun PreviewCarsList() {
-    CarsList(
-        listOf(
-            CarsUIItem(title = "2020 Mazda 3", price = "$20,000", image = null),
-            CarsUIItem(
-                title = "2019 very long long very long title very long",
-                price = "$30,000",
-                image = null
-            )
-        )
-    )
 }
 
 @Composable
---
+++ app/src/main/java/com/leonard/cars_compose/ui/CarsList.kt
@@ -0,0 +1,43 @@
+package com.leonard.cars_compose.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Divider
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.leonard.cars_compose.ui.model.CarsUIItem
+import com.leonard.cars_compose.ui.model.CarsUIList
+
+@Composable
+internal fun CarsList(list: CarsUIList) {
+    LazyColumn(
+        contentPadding = PaddingValues(16.dp),
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        items(items = list) { car ->
+            ListRow(car)
+            Divider()
+        }
+    }
+}
+
+@Composable
+@Preview
+private fun PreviewCarsList() {
+    CarsList(
+        listOf(
+            CarsUIItem(title = "2020 Mazda 3", price = "$20,000", image = null),
+            CarsUIItem(
+                title = "2019 very long long very long title very long",
+                price = "$30,000",
+                image = null
+            )
+        )
+    )
+}
